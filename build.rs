7a6d3ec9e4a172f3fb6ec5b004bbdb168740cfd8
invuzt <invuzt@gmail.com>
2026-04-15 10:49:01 +0700

    Switch to official @library material import

--- build.rs
+++ build.rs
@@ -1,5 +1,6 @@
 fn main() {
     let config = slint_build::CompilerConfiguration::new()
+        .with_library_paths([("material".to_string(), std::path::PathBuf::from("ui"))].into_iter().collect())
         .with_include_paths(vec![
             std::path::PathBuf::from("ui"),
             std::path::PathBuf::from("ui/views"),
