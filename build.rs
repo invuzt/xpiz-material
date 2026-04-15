d3c2926ca93dea1254549b8ccc3670b457b62a48
invuzt <invuzt@gmail.com>
2026-04-15 10:38:56 +0700

    Brute force fix for material.slint paths

--- build.rs
+++ build.rs
@@ -1,5 +1,9 @@
 fn main() {
     let config = slint_build::CompilerConfiguration::new()
-        .with_include_paths(vec![std::path::PathBuf::from("src")]);
+        .with_include_paths(vec![
+            std::path::PathBuf::from("ui"),
+            std::path::PathBuf::from("ui/views"),
+            std::path::PathBuf::from("ui/components")
+        ]);
     slint_build::compile_with_config("ui/main.slint", config).unwrap();
 }
